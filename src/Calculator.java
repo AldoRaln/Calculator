3dd4a254e8f6102d863169738ad74c4dd362fc5e
AldoRaln <mar.mar.dm@yandex.ru>
2022-08-30 22:03:21 +0300

    Calculator 1.2

--- src/Calculator.java
+++ src/Calculator.java
@@ -14,30 +14,30 @@ public class Calculator {
     public String calculate(String value) throws Exception {
         String[] object = value.split(" ");
         // 1.Содержит ли пример три объекта (1 знак)
-        if(!(object.length == 3 && (object[1].equals("+") || object[1].equals("-") || object[1].equals("*") || object[1].equals("/")))){
+        if (!(object.length == 3 && (object[1].equals("+") || object[1].equals("-") || object[1].equals("*") || object[1].equals("/")))) {
             throw new Exception("Пример написан не корректно");
         }
 
         // 2.Эти два объекта целые числа от 1-10
-        {
-            int first = Integer.parseInt(object[0]);
-            int second = Integer.parseInt(object[2]);
+        try {
+                int first = Integer.parseInt(object[0]);
+                int second = Integer.parseInt(object[2]);
 
-            if ((first >= 1 && first <= 10) && (second >= 1 && second <= 10)) {
-                return String.valueOf(mathOperation(first, second, object[1]));
+                if ((first >= 1 && first <= 10) && (second >= 1 && second <= 10)) {
+                    return String.valueOf(mathOperation(first, second, object[1]));
             }
-        }
+        } catch (Exception E) {System.out.println(E.getMessage());}
         // 3.Или эти два объекта римские цифры от I-X
-        int first = RomanNumerals.convertStringToInt(object[0]);
-        int second = RomanNumerals.convertStringToInt(object[2]);
-        if((first >= 1 && first <= 10) && (second >= 1 && second <= 10)) {
-            int result = mathOperation(first, second, object[1]);
-            if (result < 0) {
-                throw new Exception("B римской системе нет отрицательных чисел");
+            int first = RomanNumerals.convertStringToInt(object[0]);
+            int second = RomanNumerals.convertStringToInt(object[2]);
+            if ((first >= 1 && first <= 10) && (second >= 1 && second <= 10)) {
+                int result = mathOperation(first, second, object[1]);
+                if (result < 0) {
+                    throw new Exception("B римской системе нет отрицательных чисел");
+                }
+                return RomanNumerals.convertIntToRoman(result);
             }
-            return RomanNumerals.convertIntToRoman (result);
-        }
-
+            System.out.println("Пример написан не корректно");
         return "";
     }
     private int mathOperation(int first, int second, String operator){
